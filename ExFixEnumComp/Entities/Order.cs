bc62359884f7f8486e37910ba59c82022a70c47f
JonhnX <johnac.victor@gmail.com>
2020-10-06 18:59:13 -0300

    Exercicio POO pedidos de certo cliente

---
+++ ExFixEnumComp/Entities/Order.cs
@@ -0,0 +1,61 @@
+﻿using ExFixEnumComp.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExFixEnumComp.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order() { }
+        public Order(OrderStatus status, Client client)
+        {
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem items)
+        {
+            OrderItems.Add(items);
+        }
+        public void RemoveItem(OrderItem items)
+        {
+            OrderItems.Remove(items);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem item in OrderItems)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s1 = new StringBuilder();
+            s1.AppendLine("ORDER SUMMARY:");
+            s1.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm/ss"));
+            s1.AppendLine("Order status: " + Status);
+            s1.Append("Client: " + Client.Name);
+            s1.AppendLine("(" + Client.BirthDate.ToString("dd/MM/yyyy") + ")" + Client.Email);
+            s1.AppendLine("Order Items: ");
+            foreach(OrderItem order in OrderItems)
+            {
+                s1.Append(order.Product.Name + ", ");
+                s1.Append("$" + order.Product.Price + ", ");
+                s1.Append("Quantity: " + order.Quantity + ", ");
+                s1.AppendLine("Subtotal: " + order.SubTotal());
+            }
+            s1.Append("Total price: " + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return s1.ToString();
+        }
+    }
+}
---
+++ ExFixEnumComp/Program.cs
@@ -0,0 +1,45 @@
+﻿using ExFixEnumComp.Entities;
+using ExFixEnumComp.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace ExFixEnumComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(orderStatus, client);
+
+            Console.Write("How many items to this order? ");
+            int mi = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= mi; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string nameP = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double priceP = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantityP = int.Parse(Console.ReadLine());
+                Product product = new Product(nameP, priceP);
+                OrderItem orderItem = new OrderItem(quantityP, priceP, product);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine(order);
+        }
+    }
+}
